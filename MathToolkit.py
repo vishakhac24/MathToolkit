fb3abfd935d842fc2d2ad6ebeef48150eb1c0444
Vishakha <96491293+vishakhac24@users.noreply.github.com>
2023-08-10 22:23:59 +0530

    Initial commit with function definitions

--- MathToolkit.py
+++ MathToolkit.py
@@ -1,1 +1,19 @@
+# math module for mathematical calculations and functions.
+# counter from collections is used for counting occurrences of elements
+import math 
+from collections import Counter
 
+# Function Definitions
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
